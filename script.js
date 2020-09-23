0d70bac37ed50ef4b8c1bb42e5315ba924162a2b
Tatjana Brandl <tatjanabrandl@gmail.com>
2020-09-23 10:17:45 +0200

    Basic Arrow Functions

---
+++ script.js
@@ -0,0 +1,30 @@
+//Opdracht A
+//zet de functie om naar een arrow functie
+const ikRockArrowFunctions = () =>
+    console.log("Joe, ik rock de arrow functions!");
+
+ikRockArrowFunctions();
+
+//Opdracht B
+const fivePlusSeven = () => 5 + 7;
+
+console.log(fivePlusSeven());
+
+//Note to self nav vraag slack: vergeet niet functie aan te roepen in de terminal met console.log
+// + () achter de variabele te plaatsen en binnen de haakjes te zetten. Niet erbuiten anders krijg je de gehele functie te zien. 
+
+//Opdracht C
+const myFunction = (a, b) => a + b
+
+console.log(myFunction(10, 30));
+
+//Opdracht D
+const addFive = (a) => a + 5;
+
+console.log(addFive(15));
+
+//Opdracht E
+const createObject = () => ({ greeting: "hoi" });
+
+console.log(createObject());
+
